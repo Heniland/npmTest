70cb11e9e0c584049e9bf0a0414d18eeadb5d527
Hanoch Arega <henokalemshewa@gmail.com>
2017-10-09 13:27:29 +0300

    Update with the correct files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Car, Engine} from 'ha-auto';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  car: Car;
+
+  constructor() {
+    this.car = new Car(21680, new Engine('V8', 400), 'Chevrolet', 'Malibu', true);
+  }
 }
